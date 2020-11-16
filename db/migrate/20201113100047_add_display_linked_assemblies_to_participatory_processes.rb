69454c16f413f9727a3195e937cbd08d3ad7b93e
THIVEND <baptiste.thivend@insa-lyon.fr>
2020-11-16 07:27:54 +0100

    Add migration

---
+++ db/migrate/20201113100047_add_display_linked_assemblies_to_participatory_processes.rb
@@ -0,0 +1,5 @@
+class AddDisplayLinkedAssembliesToParticipatoryProcesses < ActiveRecord::Migration[5.2]
+  def change
+    add_column :decidim_participatory_processes, :display_linked_assemblies, :boolean, default: false
+  end
+end
