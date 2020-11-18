afcedab40f4c4a691efdf07ed2dee2d4d967cec7
Baptiste THIVEND <baptiste.thivend@insa-lyon.fr>
2020-11-18 11:06:21 +0100

    Lint files

--- db/migrate/20201113100047_add_display_linked_assemblies_to_participatory_processes.rb
+++ db/migrate/20201113100047_add_display_linked_assemblies_to_participatory_processes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddDisplayLinkedAssembliesToParticipatoryProcesses < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_participatory_processes, :display_linked_assemblies, :boolean, default: false
--- spec/system/participatory_process_steps_spec.rb
+++ spec/system/participatory_process_steps_spec.rb
@@ -11,7 +11,7 @@ describe "Participatory Processes Steps", type: :system do
       :active,
       organization: organization,
       description: { en: "Description", ca: "Descripció", es: "Descripción" },
-      short_description: { en: "Short description", ca: "Descripció curta", es: "Descripción corta" },
+      short_description: { en: "Short description", ca: "Descripció curta", es: "Descripción corta" }
     )
   end
 
@@ -35,140 +35,139 @@ describe "Participatory Processes Steps", type: :system do
                title: { en: "Active step", ca: "Fase activa", es: "Fase activa" })
       end
 
+      context "when promoted process" do
+        it "display default button" do
+          visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
 
-        context "when promoted process" do
-          it "display default button" do
+          within ".process-header__phase" do
+            expect(page).to have_content("VIEW PHASES")
+          end
+        end
+
+        context "with action button" do
+          let(:cta_text) { { en: "SEE", ca: "SEE", es: "SEE" } }
+          let(:cta_path) { "my path" }
+          let!(:active_step) do
+            create(:participatory_process_step,
+                   :active,
+                   cta_text: cta_text,
+                   cta_path: cta_path,
+                   participatory_process: promoted_process,
+                   title: { en: "Active step", ca: "Fase activa", es: "Fase activa" })
+          end
+
+          it "display custom button" do
             visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
 
             within ".process-header__phase" do
-              expect(page).to have_content("VIEW PHASES")
+              expect(page).to have_content("SEE")
             end
           end
 
-          context "with action button" do
-            let(:cta_text) { { en: "SEE", ca: "SEE", es: "SEE" } }
-            let(:cta_path) { "my path" }
-            let!(:active_step) do
-              create(:participatory_process_step,
-                     :active,
-                     cta_text: cta_text,
-                     cta_path: cta_path,
-                     participatory_process: promoted_process,
-                     title: { en: "Active step", ca: "Fase activa", es: "Fase activa" })
+          context "when action btn is nil" do
+            let(:cta_text) { nil }
+
+            it "display default button" do
+              visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
+
+              within ".process-header__phase" do
+                expect(page).to have_content("VIEW PHASES")
+              end
             end
+          end
+
+          context "when action btn is blank in current locale" do
+            let(:cta_text) { { en: "", ca: "HEY IN CATALÀ!", es: "HEY IN CASTELLANO!" } }
+
+            it "display next language defined in cta_text in button" do
+              visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
+
+              within ".process-header__phase" do
+                expect(page).to have_content("HEY IN CATALÀ!")
+              end
+            end
+          end
+
+          context "when action btn is blank in another locale" do
+            let(:cta_text) { { en: "HEY!", ca: "", es: "" } }
 
             it "display custom button" do
               visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
 
               within ".process-header__phase" do
-                expect(page).to have_content("SEE")
-              end
-            end
-
-            context "when action btn is nil" do
-              let(:cta_text) { nil }
-
-              it "display default button" do
-                visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
-
-                within ".process-header__phase" do
-                  expect(page).to have_content("VIEW PHASES")
-                end
-              end
-            end
-
-            context "when action btn is blank in current locale" do
-              let(:cta_text) { { en: "", ca: "HEY IN CATALÀ!", es: "HEY IN CASTELLANO!" } }
-
-              it "display next language defined in cta_text in button" do
-                visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
-
-                within ".process-header__phase" do
-                  expect(page).to have_content("HEY IN CATALÀ!")
-                end
-              end
-            end
-
-            context "when action btn is blank in another locale" do
-              let(:cta_text) { { en: "HEY!", ca: "", es: "" } }
-
-              it "display custom button" do
-                visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
-
-                within ".process-header__phase" do
-                  expect(page).to have_content("HEY!")
-                end
+                expect(page).to have_content("HEY!")
               end
             end
           end
         end
+      end
 
-        context "when highlighted process" do
-          it "display default button" do
+      context "when highlighted process" do
+        it "display default button" do
+          visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
+
+          within ".process-header__phase" do
+            expect(page).to have_content("VIEW PHASES")
+          end
+        end
+
+        context "with cta button" do
+          let(:cta_text) { { en: "SEE", ca: "SEE", es: "SEE" } }
+          let(:cta_path) { "my path" }
+          let!(:active_step) do
+            create(:participatory_process_step,
+                   :active,
+                   cta_text: cta_text,
+                   cta_path: cta_path,
+                   participatory_process: promoted_process,
+                   title: { en: "Active step", ca: "Fase activa", es: "Fase activa" })
+          end
+
+          it "display custom button" do
             visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
 
             within ".process-header__phase" do
-              expect(page).to have_content("VIEW PHASES")
+              expect(page).to have_content("SEE")
             end
           end
 
-          context "with cta button" do
-            let(:cta_text) { { en: "SEE", ca: "SEE", es: "SEE" } }
-            let(:cta_path) { "my path" }
-            let!(:active_step) do
-              create(:participatory_process_step,
-                     :active,
-                     cta_text: cta_text,
-                     cta_path: cta_path,
-                     participatory_process: promoted_process,
-                     title: { en: "Active step", ca: "Fase activa", es: "Fase activa" })
+          context "when cta btn is nil" do
+            let(:cta_text) { nil }
+
+            it "display default button" do
+              visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
+
+              within ".process-header__phase" do
+                expect(page).to have_content("VIEW PHASES")
+              end
             end
+          end
+
+          context "when cta btn is blank in en" do
+            let(:cta_text) { { en: "", ca: "HEY IN CATALÀ!", es: "HEY IN CASTELLANO!" } }
+
+            it "display next language defined in cta_text in button" do
+              visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
+
+              within ".process-header__phase" do
+                expect(page).to have_content("HEY IN CATALÀ!")
+              end
+            end
+          end
+
+          context "when cta btn is blank in ca" do
+            let(:cta_text) { { en: "HEY!" } }
 
             it "display custom button" do
               visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
 
               within ".process-header__phase" do
-                expect(page).to have_content("SEE")
-              end
-            end
-
-            context "when cta btn is nil" do
-              let(:cta_text) { nil }
-
-              it "display default button" do
-                visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
-
-                within ".process-header__phase" do
-                  expect(page).to have_content("VIEW PHASES")
-                end
-              end
-            end
-
-            context "when cta btn is blank in en" do
-              let(:cta_text) { { en: "", ca: "HEY IN CATALÀ!", es: "HEY IN CASTELLANO!" } }
-
-              it "display next language defined in cta_text in button" do
-                visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
-
-                within ".process-header__phase" do
-                  expect(page).to have_content("HEY IN CATALÀ!")
-                end
-              end
-            end
-
-            context "when cta btn is blank in ca" do
-              let(:cta_text) { { en: "HEY!" } }
-
-              it "display custom button" do
-                visit decidim_participatory_processes.participatory_process_participatory_process_steps_path(promoted_process)
-
-                within ".process-header__phase" do
-                  expect(page).to have_content("HEY!")
-                end
+                expect(page).to have_content("HEY!")
               end
             end
           end
         end
       end
     end
+  end
 end
