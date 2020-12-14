dfb80d41f2e07f152fb8b6b202fd94b54e38a75e
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-12-14 11:56:31 +0100

    enable multiple maps content blocks

--- app/assets/config/decidim_homepage_interactive_map_manifest.js
+++ app/assets/config/decidim_homepage_interactive_map_manifest.js
@@ -1,2 +1,3 @@
 // = link decidim/homepage_interactive_map/interactive_map.js
+// = link decidim/homepage_interactive_map/interactive_map_without_dependencies.js
 // = link decidim/homepage_interactive_map/scope.js
