8bc902c4f13eb96a6d4a5ea41c4294bb3b07ac5d
Quentin Champ <26109239+Quentinchampenois@users.noreply.github.com>
2021-04-08 23:38:48 +0200

    backport fix container error (#22)

--- app/helpers/decidim/homepage_interactive_map/interactive_map_helper.rb
+++ app/helpers/decidim/homepage_interactive_map/interactive_map_helper.rb
@@ -8,17 +8,28 @@ module Decidim
       end
 
       def interactive_map_for(geojson, css_class = "row column")
-        return if Rails.application.secrets.maps.blank?
+        return unless geolocation_enabled?
+        return if Decidim::Map.configuration[:api_key].blank?
 
         map_html_options = {
           class: "map",
           id: "interactive_map",
           "data-geojson-data" => geojson.to_json,
-          "data-here-api-key" => Rails.application.secrets.maps[:api_key]
+          "data-here-api-key" => Decidim::Map.configuration[:api_key]
         }
         content = capture { yield }.html_safe
         content_tag :div, class: css_class do
           content_tag(:div, "", map_html_options) + content
+        end
+      end
+
+      def interactive_map_script(content_blocks)
+        if content_blocks.map(&:manifest_name).include?("interactive_map") && content_blocks.map(&:manifest_name).include?("upcoming_events")
+          "decidim/homepage_interactive_map/interactive_map_without_dependencies"
+        elsif content_blocks.map(&:manifest_name).include?("interactive_map")
+          "decidim/homepage_interactive_map/interactive_map"
+        else
+          ""
         end
       end
     end
