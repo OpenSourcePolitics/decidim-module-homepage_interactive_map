92e3037b7f9c9e5c677a0fd5c903035fd0aa04dd
eliegaboriau <elie@opensourcepolitics.eu>
2022-07-11 11:36:03 +0200

    fix tests 2.0.0

--- spec/lib/decidim/homepage_interactive_map_spec.rb
+++ spec/lib/decidim/homepage_interactive_map_spec.rb
@@ -5,13 +5,13 @@ describe Decidim::HomepageInteractiveMap do
 
   describe "#version" do
     it "returns module's version" do
-      expect(described_class.version).to eq("1.0.0")
+      expect(described_class::VERSION).to eq("2.0.0")
     end
   end
 
   describe "#decidim_compatibility_version" do
     it "returns module's version" do
-      expect(described_class.decidim_compatibility_version).to eq("0.26")
+      expect(described_class::COMPAT_DECIDIM_VERSION).to eq([">= 0.25.0","< 0.27"])
     end
   end
 end
