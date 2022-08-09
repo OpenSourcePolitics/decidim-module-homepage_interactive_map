a741b907a8849f43ffbbd66e49efe786104eef25
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-08-09 14:45:51 +0200

    Downgrade Decidim version

--- lib/decidim/homepage_interactive_map/version.rb
+++ lib/decidim/homepage_interactive_map/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-meetings version.
   module HomepageInteractiveMap
     VERSION = "2.0.0"
-    DECIDIM_VERSION = "0.26.0"
-    COMPAT_DECIDIM_VERSION = [">= 0.25.0", "< 0.27"].freeze
+    DECIDIM_VERSION = "0.25.2"
+    COMPAT_DECIDIM_VERSION = [">= 0.25.0", "< 0.26"].freeze
   end
 end
