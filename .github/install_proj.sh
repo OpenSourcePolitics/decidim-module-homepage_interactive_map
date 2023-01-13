d315e3b4f4a0d3ee87c028ffc06050b2cb9f868b
armandfardeau <fardeauarmand@gmail.com>
2023-01-13 17:51:43 +0100

    Add dep

---
+++ .github/install_proj.sh
@@ -0,0 +1,9 @@
+sudo apt-get update && sudo apt install cmake sqlite libtiff-dev curl libcurl4-openssl-dev libssl-dev -y
+curl https://download.osgeo.org/proj/proj-9.1.1.tar.gz -o proj-9.1.1.tar.gz
+tar -xzf proj-9.1.1.tar.gz
+cd proj-9.1.1
+mkdir build
+cd build
+cmake ..
+cmake --build .
+sudo cmake --build . --target install
